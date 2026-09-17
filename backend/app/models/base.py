a0bd6cadcb4963916b526ee6c7d935793f0178da
dhanwadeuday-lgtm <dhanwade.uday@gmail.com>
2026-09-17 21:55:38 +0530

    Delete backend directory

--- backend/app/models/base.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, DateTime, func
-
-class Base(DeclarativeBase):
-    pass
-
-class TimestampMixin:
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())

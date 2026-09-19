29119c0a7e7d25947600133fcb231a979596db65
dhanwadeuday-lgtm <dhanwade.uday@gmail.com>
2026-09-19 14:59:23 +0530

    Delete snickylink directory

--- snickylink/backend/src/utils/ids.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-const id = (p = '') => p + crypto.randomBytes(12).toString('hex');
-const numericCode = (n = 6) =>
-  Array.from({ length: n }, () => crypto.randomInt(0, 10)).join('');
-const inviteCode = () => {
-  const alphabet = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789'; // no I/O/0/1 confusion
-  return Array.from({ length: 8 }, () => alphabet[crypto.randomInt(0, alphabet.length)]).join('');
-};
-module.exports = { id, numericCode, inviteCode };
